27264b9012cc4c29081c6c24d9a7dff1013d051d
kamal-bit-0134 <69454533+kamal-bit-0134@users.noreply.github.com>
2023-04-24 22:25:54 +0530

    initial commit

---
+++ features.js
@@ -0,0 +1,13 @@
+// const fname = "Kamal"
+// const fname1 = "Kamal1"
+// const fname2 = "Kamal2"
+
+
+
+// module.exports = fname
+// export default fname;
+// export {fname,fname1,fname2}
+
+export const generateNum = ()=>{
+    return `${~~(Math.random() * 100)}%`
+}
